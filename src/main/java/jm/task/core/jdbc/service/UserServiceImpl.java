fbad379b9de772788e66957eb85fb3d014168999
Bohtaro <bohtarovaha@gmail.com>
2022-09-07 21:16:40 +0300

    private modifier for field "UserDaoJDBCImpl userDaoJDBC".

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    private UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
         userDaoJDBC.createUsersTable();
