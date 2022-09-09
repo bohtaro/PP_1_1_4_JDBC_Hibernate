7d90843e994eb76f8031cf92196805684409dc55
Bohtaro <bohtarovaha@gmail.com>
2022-09-09 20:14:08 +0300

    private modifier for field "UserDaoJDBCImpl userDaoJDBC".

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,17 @@
 package jm.task.core.jdbc.util;
 
-import java.lang.reflect.InvocationTargetException;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.cfg.Configuration;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
@@ -11,6 +19,7 @@ public class Util {
     private static final String USER = "root";
     private static final String PASSWORD = "root";
     private static Connection connection = null;
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
 
@@ -21,5 +30,34 @@ public class Util {
             throw new RuntimeException(e);
         }
         return connection;
+    }
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, DRIVER);
+                settings.put(Environment.URL, URL);
+                settings.put(Environment.USER, USER);
+                settings.put(Environment.PASS, PASSWORD);
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+                //settings.put(Environment.SHOW_SQL, "true");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(User.class);
+
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties());
+
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+                System.out.println("ok");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 }
